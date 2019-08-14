783c15437ed7bf93067ae47409204787f87cb888
xrotwang <xrotwang@googlemail.com>
2019-08-14 08:10:25 +0200

    add simplified roundtrip read-write of sheets

--- src/pygrambank/sheet.py
+++ src/pygrambank/sheet.py
@@ -6,25 +6,10 @@ from itertools import groupby
 import unicodedata
 
 import attr
-import xlrd
-import openpyxl
 from csvw import dsv
 from clldutils.misc import lazyproperty, nfilter
 from clldutils.path import Path, as_unicode
 
-LANGUAGE_TO_GLOTTOCODE = {
-    "Guwidj [ung]": "guwi1238",
-    "Munumburru [ung]": "munu1238",
-    "Ngarnawu [ung]": "ngar1285",
-    "Waladjangari (Wurla) [ung]": "wurl1240",
-    "Woljamidi [ung]": "woly1239",
-    "Yagaria-Hua [ygr]": "huaa1250",
-    "Yagaria-Move [ygr]": "move1241",
-    "Hokkien [nan]": "hokk1242",
-    "Chaozhou [nan]": "chao1238",
-    "Kinikinao [gqn]": "huaa1250",
-    "Terena [ter]": "move1241",
-}
 
 GB_COLS = OrderedDict([
     ("Language_ID", ["iso-639-3", "Language", "Glottocode", "glottocode"]),
@@ -68,9 +53,7 @@ class NewSheet(object):
 
     @property
     def language_code(self):
-        if '[' in self.language:
-            return self.language.split('[')[1].split(']')[0].strip()
-        return self.language
+        return self.language.split('[')[1].split(']')[0].strip()
 
 
 def unicode_from_path(p):
@@ -135,110 +118,57 @@ class Sheet(object):
     """
     Processing workflow:
     """
-    valid_suffixes = ['.xlsx', '.xls', '.csv']
+    name_pattern = re.compile(
+        '(?P<coders>[A-Z]+(-[A-Z]+)*)_(?P<glottocode>[a-z0-9]{4}[0-9]{4})\.tsv$')
 
-    def __init__(self, path, glottolog, features):
-        path = Path(path)
-
-        # We support reading the sheet from the preparsed TSV file:
-        self._from_tsv = path.suffix == '.tsv'
-
-        m = re.match(
-            '(?P<coder>[^_]+)_(?P<lgname>[^\[]+)\[(?P<lgid>[^\]]+)\]\s*(\.xlsb)?$', path.stem)
-        if not m or ((not self._from_tsv) and path.suffix not in self.valid_suffixes):
-            # We can't read metadata from the filename, or the format is not supported.
-            raise ValueError(path)  # pragma: no cover
-
+    def __init__(self, path):
+        match = self.name_pattern.match(path.name)
+        assert match, 'Invalid sheet name: {0}'.format(path.name)
         self.path = path
-        if self._from_tsv:
-            for suffix in self.valid_suffixes:
-                origin = path.parent.joinpath(unicode_from_path(path.stem) + suffix)
-                if origin.exists():
-                    self.path = origin
-                    break
+        self.coders = match.group('coders').split('-')
+        self.glottocode = match.group('glottocode')
 
-        self.coder = unicode_from_path(m.group('coder').strip())
-        self.lgname = unicode_from_path(m.group('lgname').strip())
-        self.lgid = m.group('lgid').strip()
-        self.lgnamecode = '{0.lgname} [{0.lgid}]'.format(self)
-        languoid = glottolog.languoids_by_ids[self.lgid]
-        lgc = languoid.id
-        self.level = languoid.level.name
-        self.lineage = [l[1] for l in languoid.lineage]
-        if languoid.level.name == 'dialect':
-            for _, lgc, level in reversed(languoid.lineage):
-                if level.name == 'language':
-                    break
-            else:
-                lgc = languoid.id
-        language = glottolog.languoids_by_glottocode[lgc]
-        self.language_id = language.id
+    def iterrows(self):
+        for row in dsv.reader(self.path, delimiter='\t', encoding='utf-8-sig', dicts=True):
+            yield row
 
-        # Macroareas are assigned to language level nodes:
-        self.macroarea = language.macroareas[0].name,
-        self.latitude = languoid.latitude if languoid.latitude else language.latitude
-        self.longitude = languoid.longitude if languoid.longitude else language.longitude
+    def visit(self, row_visitor=None):
+        if row_visitor is None:
+            row_visitor = lambda r: r
+        rows = list(self.iterrows())
+        with dsv.UnicodeWriter(self.path, delimiter='\t', encoding='utf8') as w:
+            for i, row in enumerate(rows):
+                if i == 0:
+                    w.writerow(list(row.keys()))
+                row = row_visitor(row)
+                if row:
+                    w.writerow(list(row.values()))
 
-        self.family_name = languoid.lineage[0][0] if languoid.lineage else None
-        self.family_id = languoid.lineage[0][1] if languoid.lineage else None
-        self.glottocode = LANGUAGE_TO_GLOTTOCODE.get(self.lgnamecode, languoid.id)
-        self._features = features
+    def check(self, api):
+        res = []
+        for row in self.iterrows():
+            row = self._normalized_row(row)
+            if row is None:
+                continue
+            res.append(row)
 
-    @lazyproperty
-    def rows(self):
-        if self._from_tsv:
-            res = []
-            for row in dsv.reader(
-                self.path.parent / (unicode_from_path(self.path.stem) + '.tsv'),
-                delimiter='\t',
-                encoding='utf-8-sig',
-                dicts=True
-            ):
-                if self.path.suffix == '.tsv':
-                    # This TSV was *not* created from an original sheet, so needs to be normalized
-                    # now!
-                    row = self._normalized_row(row)
-                    if row is None:
-                        continue
-                res.append(row)
-
-            dedupres = []
-            for gbid, rows in groupby(
-                sorted(res, key=lambda r: r['Feature_ID']), lambda r: r['Feature_ID']
-            ):
-                rows = list(rows)
-                if len(rows) > 1:
-                    # A feature is coded multiple times! If the codings are inconsistent, we raise
-                    # an error, otherwise the first value takes precedence.
-                    if len(set(r['Value'] for r in rows)) > 1:
-                        print(
-                            'ERROR:{0}:{1} inconsistent multiple codings: {2}'.format(
-                                self.path.name, gbid, [r['Value'] for r in rows]))
-                    else:
-                        dedupres.append(rows[0])
+        dedupres = []
+        for gbid, rows in groupby(
+            sorted(res, key=lambda r: r['Feature_ID']), lambda r: r['Feature_ID']
+        ):
+            rows = list(rows)
+            if len(rows) > 1:
+                # A feature is coded multiple times! If the codings are inconsistent, we raise
+                # an error, otherwise the first value takes precedence.
+                if len(set(r['Value'] for r in rows)) > 1:
+                    print(
+                        'ERROR:{0}:{1} inconsistent multiple codings: {2}'.format(
+                            self.path.name, gbid, [r['Value'] for r in rows]))
                 else:
                     dedupres.append(rows[0])
-            return dedupres
-        _rows = {
-            '.xlsx': self._xlsx_to_rows,
-            '.xls': self._xls_to_rows,
-            '.csv': self._csv_to_rows}[self.path.suffix]()
-        return nfilter(self._normalized_row(OrderedDict(zip(_rows[0], row))) for row in _rows[1:])
-
-    def write_tsv(self, restrict_cols=False, fn=None):
-        try:
-            fn = fn or self.path.parent / (unicode_from_path(self.path.stem) + '.tsv')
-        except UnicodeDecodeError:
-            print([fn, self.path.parent, self.path.stem])
-            fn = fn or self.path.parent / (self.path.stem + '.tsv')
-        with dsv.UnicodeWriter(fn, delimiter='\t') as w:
-            for i, row in enumerate(self.rows):
-                if i == 0:
-                    w.writerow(list(GB_COLS.keys()) if restrict_cols else list(row.keys()))
-                w.writerow(
-                    [row.get(col, '') for col in GB_COLS.keys()]
-                    if restrict_cols else list(row.values()))
-                yield row
+            else:
+                dedupres.append(rows[0])
+        return dedupres
 
     def _normalized_row(self, row):
         for k in row:
@@ -268,72 +198,3 @@ class Sheet(object):
         row['Value'] = normalized_value(self, row['Value'], self._features[row['Feature_ID']])
         row['Comment'] = normalize_comment(row['Comment'])
         return row
-
-    #
-    # Helper methods to read various tabular file formats.
-    #
-    @staticmethod
-    def _read_excel_value(x):
-        if x is None:
-            return ""
-        if type(x) == type(0.0):
-            return '{0}'.format(int(x))
-        return '{0}'.format(x).strip()
-
-    def _xlsx_to_rows(self):
-        import warnings
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore')
-            sheet = openpyxl.load_workbook(str(self.path), data_only=True).active
-        rows = []
-        empty_rows = 0
-        skip_cols = set()
-        for i, row in enumerate(sheet.rows):
-            if i == 0:
-                for j, c in enumerate(row):
-                    # There's a couple of sheets with > 10,000 columns, labeled with "Column<no>".
-                    # We cut these out to reduce TSV bloat.
-                    if re.match('Column[0-9]+$', '{0}'.format(c.value)):
-                        skip_cols.add(j)
-            row = [self._read_excel_value(c.value) for j, c in enumerate(row) if j not in skip_cols]
-            if set(row) == {''}:  # pragma: no cover
-                empty_rows += 1
-                if empty_rows > 1000:
-                    # There's a couple of sheets with > 100,000 (mostly empty) rows.
-                    # After encountering more than 1,000, we stop reading.
-                    break
-                else:
-                    continue
-            rows.append(row)
-        return rows
-
-    def _xls_to_rows(self):
-        wb = xlrd.open_workbook(str(self.path))
-        rows_by_sheetname = defaultdict(list)
-        for sheet in wb.sheets():  # We read all sheets in the workbook.
-            for row in range(sheet.nrows):
-                rows_by_sheetname[sheet.name].append(
-                    [self._read_excel_value(sheet.cell_value(row, col))
-                     for col in range(sheet.ncols)])
-        # Now select the proper sheet:
-        if len(rows_by_sheetname) > 1:
-            for sheetname in ["GramBank", '"Empty" GramBank Sheet']:
-                if sheetname in rows_by_sheetname:
-                    return rows_by_sheetname[sheetname]
-        return list(rows_by_sheetname.values())[0]
-
-    def _csv_to_rows(self):
-        def read(encoding):
-            with self.path.open(encoding=encoding) as csvfile:
-                line = csvfile.readline()
-                delimiter = ','
-                if ';' in line and ((',' not in line) or (line.index(';') < line.index(','))):
-                    delimiter = ';'
-            spamreader = dsv.reader(
-                self.path, delimiter=delimiter, quotechar='"', doublequote=True, encoding=encoding)
-            return [row for row in spamreader]
-
-        try:
-            return read('utf-8-sig')
-        except UnicodeDecodeError:  # pragma: no cover
-            return read('cp1252')
