41a8bec6f39b1c5ee64cdb6283b46d8c64c07d82
xrotwang <xrotwang@googlemail.com>
2019-09-19 10:29:26 +0200

    release 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygrambank',
-    version='0.1.1.dev0',
+    version='0.2.0',
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
     description='A python library to access Grambank data',
--- src/pygrambank/__init__.py
+++ src/pygrambank/__init__.py
@@ -1,3 +1,3 @@
 from pygrambank.api import Grambank  # noqa: F401
 
-__version__ = '0.1.1.dev0'
+__version__ = '0.2.0'
