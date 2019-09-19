8873fd58ea9cc56e1a8c0481e14148599a047684
xrotwang <xrotwang@googlemail.com>
2019-09-19 10:31:30 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygrambank',
-    version='0.2.0',
+    version='0.2.1.dev0',
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
     description='A python library to access Grambank data',
