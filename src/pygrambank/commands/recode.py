d018a646b6985ed7f510e32ec8fa0d80e07fad05
Robert Forkel <xrotwang@googlemail.com>
2021-05-20 08:52:20 +0200

    add option to fix mixed cp1252 / utf8 encodings

--- src/pygrambank/commands/recode.py
+++ src/pygrambank/commands/recode.py
@@ -9,11 +9,29 @@ def register(parser):
     parser.add_argument(
         '--encoding',
         default='cp1252',
-        choices=['cp1252', 'macroman'],
+        choices=['cp1252', 'macroman', 'mixed'],
     )
 
 
 def run(args):
     for p in args.path:
-        c = p.read_text(encoding=args.encoding)
+        if args.encoding == 'mixed':
+            # Dunno how this happens, but sometimes, different lines are
+            # encoded differently ...
+            c = []
+            for line in p.read_bytes().split(b'\n'):
+                l = []
+                for chunk in line.split(b'\t'):
+                    try:
+                        l.append(chunk.decode('utf8'))
+                    except:
+                        try:
+                            l.append(chunk.decode('cp1252'))
+                        except:
+                            print(chunk)
+                            raise
+                c.append('\t'.join(l))
+            c = '\n'.join(c)
+        else:
+            c = p.read_text(encoding=args.encoding)
         p.write_text(c, encoding='utf8')
