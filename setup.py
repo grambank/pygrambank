bf0f34d7f5611182eb520a15fadadeff0650047d
Robert Forkel <xrotwang@googlemail.com>
2021-12-08 08:04:58 +0100

    release 2.0.0

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygrambank',
-    version='1.1.1.dev0',
+    version='2.0.0',
     author='Robert Forkel',
-    author_email='forkel@shh.mpg.de',
-    description='A python library to access Grambank data',
+    author_email='robert_:forkel@eva.mpg.de',
+    description='A python library to curate Grambank data',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     keywords='',
@@ -56,6 +56,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
