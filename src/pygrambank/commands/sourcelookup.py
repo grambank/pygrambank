234f21bfd83518df12474ffe2c637a1a8e6dfc50
Johannes Englisch <johannes_englisch@eva.mpg.de>
2022-07-19 13:25:01 +0200

    sourcelookup: allow more than one input sheet (cf. #70)

--- src/pygrambank/commands/sourcelookup.py
+++ src/pygrambank/commands/sourcelookup.py
@@ -8,7 +8,7 @@ from termcolor import colored
 from cldfcatalog import Catalog
 
 from pygrambank.sheet import Sheet
-from pygrambank.cldf import refs
+from pygrambank.cldf import refs, Glottolog, Bibs, languoid_id_map
 
 
 def register(parser):
@@ -19,8 +19,9 @@ def register(parser):
         type=pathlib.Path,
     )
     parser.add_argument(
-        'sheet',
+        'sheets',
         type=pathlib.Path,
+        nargs='+',
     )
     parser.add_argument(
         '--glottolog-version',
@@ -38,29 +39,49 @@ def run(args):
 
 
 def run_(args, glottolog):  # pragma: no cover
-    sources, unresolved, lgks = refs(args.repos, glottolog, Sheet(args.sheet))
-    seen = collections.defaultdict(list)
-    print(colored('Resolved sources:', attrs=['bold']))
-    for src in sources:
-        seen[src.id].append(src)
-    for srcid, srcs in seen.items():
-        print('{}\t{}\t{}'.format(len(srcs), srcid, srcs[0]))
-    if unresolved:
-        print()
-        print(colored('Unresolved sources:', attrs=['bold']))
-        for spec, v in unresolved.most_common():
-            try:
-                author, year, code = spec
-                print('{}\t{} {}'.format(v, author, year))
-            except ValueError:
-                print(spec)
-        if lgks:
+    sheets = [Sheet(sh) for sh in args.sheets]
+
+    print('Reading languoid ids from Glottolog...')
+    glottolog = Glottolog(glottolog)
+    id_to_glottocode = languoid_id_map(glottolog, [s.glottocode for s in sheets])
+
+    print('Loading bibliography...')
+    bibs = Bibs(glottolog, args.repos)
+
+    keys_by_glottocode = collections.defaultdict(set)
+    for key, code in bibs.iter_codes():
+        if code in id_to_glottocode:
+            keys_by_glottocode[id_to_glottocode[code]].add(key)
+
+    for sheet in sheets:
+        print(colored(
+            '\nSource look-up for sheet {}...\n'.format(sheet.path),
+            attrs=['bold']))
+        sources, unresolved, lgks = refs(
+            args.repos, sheet.glottocode, bibs, keys_by_glottocode, sheet)
+
+        seen = collections.defaultdict(list)
+        print(colored('Resolved sources:', attrs=['bold']))
+        for src in sources:
+            seen[src.id].append(src)
+        for srcid, srcs in seen.items():
+            print('{}\t{}\t{}'.format(len(srcs), srcid, srcs[0]))
+        if unresolved:
             print()
-            print(colored('Available sources:', attrs=['bold']))
-            for (k, t, a, y) in lgks:
-                print('{}\t{}\t{}'.format(
-                    colored(k, color='blue'),
-                    t,
-                    colored('{} {}'.format(a, y), attrs=['bold'])))
-    print()
-    print(colored('FAIL' if unresolved else 'OK', color='red' if unresolved else 'green'))
+            print(colored('Unresolved sources:', attrs=['bold']))
+            for spec, v in unresolved.most_common():
+                try:
+                    author, year, code = spec
+                    print('{}\t{} {}'.format(v, author, year))
+                except ValueError:
+                    print(spec)
+            if lgks:
+                print()
+                print(colored('Available sources:', attrs=['bold']))
+                for (k, t, a, y) in lgks:
+                    print('{}\t{}\t{}'.format(
+                        colored(k, color='blue'),
+                        t,
+                        colored('{} {}'.format(a, y), attrs=['bold'])))
+        print()
+        print(colored('FAIL' if unresolved else 'OK', color='red' if unresolved else 'green'))
