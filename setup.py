8b50af59a5e9dc4f420619d8535c3de2ea50a954
Johannes Englisch <johannes_englisch@eva.mpg.de>
2022-11-04 09:35:15 +0100

    update csvw version requirement

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'clldutils>=3.6',
         'cldfcatalog',
-        'csvw>=1.10',
+        'csvw>=3.1.3',
         'pycldf>=1.29',
         'xlrd',
         'openpyxl==2.4.8',
