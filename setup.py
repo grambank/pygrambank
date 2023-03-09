b98165c8a2819404e409fec6a943842a83259a75
Johannes Englisch <johannes_englisch@eva.mpg.de>
2023-03-09 11:31:48 +0100

    update ci

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         ],
     },
     platforms='any',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'clldutils>=3.6',
         'cldfcatalog',
@@ -52,11 +52,11 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
